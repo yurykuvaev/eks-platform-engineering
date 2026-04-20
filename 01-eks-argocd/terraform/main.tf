aff7002896da847396c73b3060f6ca087936bbf1
Yury Kuvaev <kuvaevdevops@gmail.com>
2026-04-20 10:42:18 -0400

    Add EKS cluster module with managed node group and IRSA

--- 01-eks-argocd/terraform/main.tf
+++ 01-eks-argocd/terraform/main.tf
@@ -38,3 +38,36 @@ module "vpc" {
 
   tags = local.tags
 }
+
+# --- EKS cluster ---
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.24"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+      subnet_ids = module.vpc.public_subnets
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = local.tags
+}
